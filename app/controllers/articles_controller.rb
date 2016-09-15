64a13b74b72a731bfe0c2297f02bef6eb7853ad7
andysmith <andys_81@hotmail.com>
2016-09-15 10:10:45 +0800

    added user login using clearance gem

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		byebug
+	  @article = Article.new(article_params)
+	 
+	  @article.save
+	  redirect_to @article
+	end
+
+	def show 
+	end 
+
+	private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+
+end
