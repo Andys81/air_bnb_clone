9cd88c6c271f6173925a5bd1d42bc8fa80b5765a
andysmith <andys_81@hotmail.com>
2016-09-21 10:40:36 +0800

    commit before adding listings feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'listings/index'
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
